7dc32993b6959665ae3fb65c909d2d6cf40d412f
Eudes SA <eudes852@hotmail.com>
2019-06-25 16:47:51 -0300

    Versão 1.0

--- app/src/main/java/com/example/carfuel/QuantidadeLitros.java
+++ app/src/main/java/com/example/carfuel/QuantidadeLitros.java
@@ -3,11 +3,14 @@ package com.example.carfuel;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.AlertDialog;
+import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.ads.internal.client.ThinAdSizeParcel;
 import com.google.android.gms.maps.SupportMapFragment;
 
 import java.text.DecimalFormat;
@@ -36,20 +39,32 @@ public class QuantidadeLitros extends AppCompatActivity implements View.OnClickL
 
     public void onClick(View v) {
         int id = v.getId();
-        if (id == R.id.button_CALC_QuantidadeLitros) {
-            //Logica do Botão
-            float valorabastecido = Float.parseFloat(valorAbastecido.getText().toString());
-            float precocombustivel = Float.parseFloat(precoCombustivel.getText().toString());
-            DecimalFormat formatador = new DecimalFormat("0.00");
-            float resultado = valorabastecido / precocombustivel;
-            //Toast.makeText(this,"A quantidade de Litros Abastecido é: " + resultado + " Litros",Toast.LENGTH_LONG).show();
+        if (valorAbastecido.getText().toString().equals("")){
+            Toast.makeText(this,"Insira o valor no Campo Valor Abastecido",Toast.LENGTH_LONG).show();
+        }
+        else if (precoCombustivel.getText().toString().equals("")){
+            Toast.makeText(this,"Insira o valor no Campo Preço do Combustível",Toast.LENGTH_LONG).show();
+        }
 
-            AlertDialog.Builder builder = new AlertDialog.Builder(this);
-            builder.setTitle("");
-            builder.setMessage("A quantidade de Litros Abastecido é: " + formatador.format(resultado) + " Litros");
-            builder.setPositiveButton("OK", null);
-            AlertDialog dialog = builder.create();
-            dialog.show();
+        else if (id == R.id.button_CALC_QuantidadeLitros) {
+            //Logica do Botão
+            try {
+                float valorabastecido = Float.parseFloat(valorAbastecido.getText().toString());
+                float precocombustivel = Float.parseFloat(precoCombustivel.getText().toString());
+                DecimalFormat formatador = new DecimalFormat("0.00");
+                float resultado = valorabastecido / precocombustivel;
+                //Toast.makeText(this,"A quantidade de Litros Abastecido é: " + resultado + " Litros",Toast.LENGTH_LONG).show();
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                builder.setTitle("");
+                builder.setMessage("A quantidade de Litros Abastecido é: " + formatador.format(resultado) + " Litros");
+                builder.setPositiveButton("OK", null);
+                AlertDialog dialog = builder.create();
+                dialog.show();
+
+            }catch (Exception errorbyzero){
+                Toast.makeText(this,"Não Podemos Dividir por ZERO",Toast.LENGTH_LONG).show();
+            }
             }
     }
 
